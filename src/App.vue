b1af94e3bcce23b624163eebe5d20a2a100658a1
chross <matiasmed.14@gmail.com>
2024-10-22 01:05:55 -0300

    feat: testing options API

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      race: "cat",
+      tasks: ['poop', 'talk', 'eat the homework'],
+      link: 'https://matiasm.com'
+    }
+  },
+  methods: {
+    toggleRace() {
+      this.race = this.race === 'cat' ? 'dog' : 'cat'
+    }
+  }
+}
+</script>
+
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <h1>{{ name }}</h1>
+  <p v-if="race === 'cat'">User is a cat! :3</p>
+  <p v-else-if="race === 'dog'">User is a dog</p>
+  <p v-else>Unknown race</p>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a :href="link" target="_blank" rel="noopener">Link to my portfolio!</a>
+  <br />
+  <button @click="toggleRace">Change race</button>
 </template>
+<!-- <template>
+  <h1 class="text-2xl">Vue Jobs</h1>
+</template> -->
+
+<style scoped>
+</style>
