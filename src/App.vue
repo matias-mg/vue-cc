4f8d7ab0a1bd0fd46acf199a5a622592bcdc9419
chross <matiasmed.14@gmail.com>
2024-10-22 20:19:56 -0300

    feat: add HomeCards component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 <template>
   <Navbar />
   <Hero />
+  <HomeCards />
 </template>
